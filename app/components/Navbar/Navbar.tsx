7f6fc20f9c632a360e79c9862995c1ad4c450859
oscarsangpa <oscarsangrador@gmail.com>
2023-04-13 20:37:23 +0200

    feat: detect current user in navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,11 +1,16 @@
 'use client';
-
+import { User } from '@prisma/client';
 import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 
-export default function Navbar() {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  console.log({ currentUser });
   return (
     <nav className="fixed w-full bg-white z-10 shadow-sm">
       <div
@@ -33,4 +38,6 @@ export default function Navbar() {
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar/Navbar';
 import Modal from './components/Modals/Modal';
 import './globals.css';
 import RegisterModal from './components/Modals/LoginModal';
-import ToastProvider from './providers/ToasterProvider';
 import ToasterProvider from './providers/ToasterProvider';
 import LoginModal from './components/Modals/LoginModal';
+import { getCurrentUser } from './actions/getCurrentUser';
 
 export const metadata = {
   title: 'Airnbnb',
@@ -16,18 +16,19 @@ const font = Nunito({
   subsets: ['latin'],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
         <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>

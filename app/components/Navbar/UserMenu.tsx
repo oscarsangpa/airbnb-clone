275dae7eab5448a16569a6d6e413fcaaeb57a9d2
oscarsangpa <oscarsangrador@gmail.com>
2023-04-17 19:13:25 +0200

    feat: change User prisma type for new type SafeUser

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { User } from '@prisma/client';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import React, { useCallback, useState } from 'react';
@@ -7,9 +6,10 @@ import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {

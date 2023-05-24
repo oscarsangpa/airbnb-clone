2ec0747578a65dc19ed0291bea6b4a545df14927
oscarsangpa <oscarsangrador@gmail.com>
2023-05-24 20:00:58 +0200

    feat: add more categories and separate in other file

--- app/components/Navbar/Categories.tsx
+++ app/components/Navbar/Categories.tsx
@@ -1,29 +1,8 @@
 'use client';
-
+import { categories } from '@/app/constants/categories';
 import Container from '../Container';
-import { TbBeach } from 'react-icons/tb';
-import { GiWindmill } from 'react-icons/gi';
-import { MdOutlineVilla } from 'react-icons/md';
 import CategoryBox from '../CategoryBox';
 import { usePathname, useSearchParams } from 'next/navigation';
-
-export const categories = [
-  {
-    label: 'Beach',
-    icon: TbBeach,
-    description: 'This property is close to the beach!',
-  },
-  {
-    label: 'Windmills',
-    icon: GiWindmill,
-    description: 'This property has windmills!',
-  },
-  {
-    label: 'Modern',
-    icon: MdOutlineVilla,
-    description: 'This property is modern!',
-  },
-];
 
 const Categories = () => {
   const params = useSearchParams();

737b1373575a832fa1b7e9a534c1a58d48209aed
oscarsangpa <oscarsangrador@gmail.com>
2023-05-24 19:31:51 +0200

    feat: when click categories, add underline style

--- app/components/Navbar/Categories.tsx
+++ app/components/Navbar/Categories.tsx
@@ -5,6 +5,7 @@ import { TbBeach } from 'react-icons/tb';
 import { GiWindmill } from 'react-icons/gi';
 import { MdOutlineVilla } from 'react-icons/md';
 import CategoryBox from '../CategoryBox';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const categories = [
   {
@@ -25,6 +26,14 @@ export const categories = [
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+
+  const isMainPage = pathname === '/';
+
+  if (!isMainPage) return null;
+
   return (
     <Container>
       <div
@@ -41,7 +50,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
           />
         ))}
